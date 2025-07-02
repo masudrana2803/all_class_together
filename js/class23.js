4424740562aa2ee9362d1f9df33ea281acc9502d
masudrana2803 <masudrana2803@gmail.com>
2025-07-03 00:59:00 +0600

    edited

--- js/class23.js
+++ js/class23.js
@@ -8,12 +8,38 @@ t1b.addEventListener('click',()=>{
 
 
 let t2h = document.querySelector('.t2h')
-let t2b = document.querySelector('t2b')
+let t2b = document.querySelector('.t2b')
 
 t2b.addEventListener('click',()=>{
-    t2h.style='color:red'
+    t2h.style='color:green'
+    t2h.innerHTML="Task 2 completed"
     console.log("I am changed")
 })
+
+
+let task3 = document.querySelector('.task3')
+let yel = document.querySelector('#yel')
+let gre = document.querySelector('#gre')
+let pin = document.querySelector('#pin')
+let pur = document.querySelector('#pur')
+let res = document.querySelector('#res')
+yel.addEventListener('click', ()=>{
+    task3.style='background:yellow;transition:1s'
+})
+gre.addEventListener('click', ()=>{
+    task3.style='background:green;transition:1s'
+})
+pin.addEventListener('click', ()=>{
+    task3.style='background:pink;transition:1s'
+})
+pur.addEventListener('click', ()=>{
+    task3.style='background:purple;transition:1s'
+})
+res.addEventListener('click', ()=>{
+    task3.style='background:black;transition:1s'
+})
+
+
 
 // console.log("hi")
 
