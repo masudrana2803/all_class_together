c9ce711c3940931a22aaeecbdce84318a08fc3df
masudrana2803 <masudrana2803@gmail.com>
2025-07-02 22:32:02 +0600

    edited

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 
 
 
-let t1h1 = document.querySelector('.t1h1')
-let t1b1 = document.querySelector('.t1b1')
+// let t1h = document.querySelector('.t1h1')
+// let t1b = document.querySelector('.t1b1')
 
-t1b1.addEventListener('click',()=>{
-  t1h1.style= 'color:yellow'
+// t1b.addEventListener('click',()=>{
+//   t1h.style= 'color:yellow'
+// })
+
+
+let alu = document.querySelector('.t1h1')
+let alu2 = document.querySelector('.t1b1')
+
+alu2.addEventListener('click',()=>{
+  alu.style='color.green'
 })
-
-
