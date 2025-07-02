e0e83a113a20f4c20f7a3de0e76aca31e2567665
masudrana2803 <masudrana2803@gmail.com>
2025-07-03 00:23:53 +0600

    edited

--- js/class23.js
+++ js/class23.js
@@ -1,12 +1,19 @@
-let alu = document.querySelector('.t1h1')
-let alu2 = document.querySelector('.t1b1')
+let t1h = document.querySelector('.t1h')
+let t1b = document.querySelector('.t1b')
 
-alu2.addEventListener('click',()=>{
-    alu.style='color:red;background:Yellow'
-
+t1b.addEventListener('click',()=>{
+    t1h.style='color:green'
     console.log("I am clicked")
 })
 
+
+let t2h = document.querySelector('.t2h')
+let t2b = document.querySelector('t2b')
+
+t2b.addEventListener('click',()=>{
+    t2h.style='color:red'
+    console.log("I am changed")
+})
 
 // console.log("hi")
 
--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 
 
 
+let alu = document.querySelector('.t1h1')
+let alu2 = document.querySelector('.t1b1')
+
+alu2.addEventListener('click',()=>{
+    alu.style='color:red;background:Yellow'
+
+    console.log("I am clicked")
+})
+
+
 // let t1h = document.querySelector('.t1h1')
 // let t1b = document.querySelector('.t1b1')
 
@@ -10,9 +20,10 @@
 // })
 
 
-let alu = document.querySelector('.t1h1')
-let alu2 = document.querySelector('.t1b1')
+// let alu = document.querySelector('.t1h1')
+// let alu2 = document.querySelector('.t1b1')
 
-alu2.addEventListener('click',()=>{
-  alu.style='color.green'
-})
+// alu2.addEventListener('click',()=>{
+//   alu.style='color:green'
+//   console.log("I am clicked")
+// })
