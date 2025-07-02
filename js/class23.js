6034c916469a9c5b43b064c8abb964eae1fc4d9c
masudrana2803 <masudrana2803@gmail.com>
2025-07-02 23:07:33 +0600

    edited

---
+++ js/class23.js
@@ -0,0 +1,20 @@
+let alu = document.querySelector('.t1h1')
+let alu2 = document.querySelector('.t1b1')
+
+alu2.addEventListener('click',()=>{
+    alu.style='color:red;background:Yellow'
+
+    console.log("I am clicked")
+})
+
+
+// console.log("hi")
+
+// let alu = document.querySelector('h1')
+// let alu2 = document.querySelector('button')
+
+// console.log="hello";
+
+// alu.addEventListener('click', ()=>{
+//     h1.style=('color:red');
+// })
