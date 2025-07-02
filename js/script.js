fc9a3080ec4afcf5a0dc3872e4127c0dc44be837
masudrana2803 <masudrana2803@gmail.com>
2025-07-02 22:08:01 +0600

    edited

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
   AOS.init();
 
 
+
+let t1h1 = document.querySelectorAll('#task1 h1')
+let t1b1 = document.querySelectorAll('#task1 button')
+
+
+
+// let t1h1 = document.querySelector('#task1 h1')
+// let t1b1 = document.querySelector('#task1 button')
+
+// t1b1.addEventListener('click',()=>{
+// t1h1.style = 'color:red'
+
+
+// })
