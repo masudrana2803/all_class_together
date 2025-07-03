0c49e475b0de89c4ddba260655a4052df75d1f16
masudrana2803 <masudrana2803@gmail.com>
2025-07-04 00:01:22 +0600

    edited

--- js/class23.js
+++ js/class23.js
@@ -1,3 +1,5 @@
+// -------------task1 start------------------
+
 let t1h = document.querySelector('.t1h')
 let t1b = document.querySelector('.t1b')
 
@@ -6,6 +8,8 @@ t1b.addEventListener('click',()=>{
     console.log("I am clicked")
 })
 
+// -------------task1 finish------------------
+// -------------task2 start------------------
 
 let t2h = document.querySelector('.t2h')
 let t2b = document.querySelector('.t2b')
@@ -15,7 +19,8 @@ t2b.addEventListener('click',()=>{
     t2h.innerHTML="Task 2 completed by Maayush"
     console.log("I am changed")
 })
-
+// -------------task2 finish------------------
+// -------------task3 start------------------
 
 let task3 = document.querySelector('.task3')
 let yel = document.querySelector('#yel')
@@ -39,9 +44,14 @@ res.addEventListener('click', ()=>{
     task3.style='background:black;transition:1s'
 })
 
+// -------------task3 finish------------------
+// -------------task4 start------------------
+
+
 let addnum = document.querySelector('.addnum')
 let subnum = document.querySelector('.subnum')
 let result = document.querySelector('.result')
+let error = document.querySelector('.error')
 let number = 1
 
 addnum.addEventListener('click', ()=>{
@@ -60,15 +70,60 @@ subnum.addEventListener('click', ()=>{
     }
 })
 
+// -------------task4 finish------------------
+// -------------task5 start------------------
 
 let input = document.querySelector('.input')
 let shbtn = document.querySelector('.shbtn')
 let shrlst = document.querySelector('.shrslt')
+let erro = document.querySelector('.erro')
 
-shbtn.addEventListener('click',()=>{
-    shrlst.innerHTML=(input.value)
+shbtn.addEventListener('click', ()=>{
+    if (input.value=='') {
+        shrlst.innerHTML=''
+        erro.innerHTML=('field cannot be blank')
+    }
+    else{
+        erro.innerHTML=''
+        shrlst.innerHTML=(input.value)
+    }
 })
 
+// -------------task5 finish------------------
+// -------------task6 start------------------
+
+let nameinput = document.querySelector('.nameinput')
+let shtxt = document.querySelector('.shtxt')
+let shorslt = document.querySelector('.shorslt')
+let errortsk6 = document.querySelector('.errortsk6')
+let tsk6bt1 = document.querySelector('.tsk6b1')
+let tsk6bt2 = document.querySelector('.tsk6b2')
+let tsk6bt3 = document.querySelector('.tsk6b3')
+
+
+
+shtxt.addEventListener('click',()=>{
+    if (nameinput.value=='') {
+        shorslt.innerHTML=''
+        errortsk6.innerHTML=('You must enter value')
+    }else{
+        errortsk6.innerHTML=''
+        shorslt.innerHTML=(nameinput.value)
+    }
+})
+
+
+tsk6bt1.addEventListener('click',()=>{
+    shorslt.style = 'color:red'
+})
+
+tsk6bt2.addEventListener('click',()=>{
+    shorslt.style = 'color:blue'
+})
+
+tsk6bt3.addEventListener('click',()=>{
+    shorslt.style = 'color:green'
+})
 
 // console.log("hi")
 
