394a37f944061fad3d4307f74070ea197d60683b
masudrana2803 <masudrana2803@gmail.com>
2025-07-09 20:42:56 +0600

    time-tables done

--- js/class23.js
+++ js/class23.js
@@ -178,3 +178,19 @@ let handelButton = ()=>{
     }
     
 }
+// -------------task8 finish------------------
+// -------------task9 start-------------------
+let input1 = document.querySelector('.input1')
+let result2= document.querySelector('.result2')
+
+let handelButtton=()=>{
+   result2.innerHTML= ''
+    if(input1.value==''){
+      alert('fill the box')
+    }
+    else{
+      for(let i=1;i<11;i++){ 
+        result2.innerHTML+= (input1.value+ 'X' + i + '='+ input1.value*i + '<br>')
+      }
+    }
+}
