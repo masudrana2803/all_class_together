a077adf7cb3316d207d608e1dd45d9b96042b971
masudrana2803 <masudrana2803@gmail.com>
2025-07-13 20:56:35 +0600

    time-tables done

--- js/guessgame.js
+++ js/guessgame.js
@@ -1,6 +1,16 @@
 let input1 = document.querySelector('.pl1')
-let p1g = document.querySelector('button')
+let input2 = document.querySelector('.pl2')
+let input3 = document.querySelector('.pl3')
+let p1g = document.querySelector('.p1g')
+let p2g = document.querySelector('.p2g')
+let p3g = document.querySelector('.p3g')
 
 p1g.addEventListener('click',()=>{
-    console.log("I am clicked")
+    prompt("Please set a value first")
+})
+p2g.addEventListener('click',()=>{
+    prompt("I am for p2")
+})
+p3g.addEventListener('click',()=>{
+    prompt("I am for p3")
 })
