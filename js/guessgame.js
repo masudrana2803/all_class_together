433e7dd8035c9fa2c4881506e9480bac49eda446
masudrana2803 <masudrana2803@gmail.com>
2025-07-13 22:57:16 +0600

    time-tables done

--- js/guessgame.js
+++ js/guessgame.js
@@ -5,8 +5,13 @@ let p1g = document.querySelector('.p1g')
 let p2g = document.querySelector('.p2g')
 let p3g = document.querySelector('.p3g')
 let error = document.querySelector('.error')
+let mainhead = document.querySelector('.mainhead')
 let pl1 = document.querySelector('.pl1')
-
+let pl2 = document.querySelector('.pl2')
+let pl3 = document.querySelector('.pl3')
+let player1 = document.querySelector('.player1')
+let player2 = document.querySelector('.player2')
+let player3 = document.querySelector('.player3')
 
 
 
@@ -18,11 +23,15 @@ p1g.addEventListener('click',()=>{
   }else{
     if (pl1.value<=10) {
         error.innerHTML=''
-        error.innerHTML=''
         console.log(pl1.value)
-        pl1.value=''
-    } else {
+        player1.style='display:none'
+        player2.style='display:block'
+        pl2.value=''
+        mainhead.innerHTML=("Player 2")
         
+    } else {
+        error.innerHTML='Set a value between 0-10'
+
     }
   }
 })
