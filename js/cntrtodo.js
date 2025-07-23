15bd9462bb65a48ec22e4e76c652458a06729bd8
masudrana2803 <masudrana2803@gmail.com>
2025-07-24 02:31:30 +0600

    perfect counter

--- js/cntrtodo.js
+++ js/cntrtodo.js
@@ -1,51 +1,51 @@
-let qwe = document.querySelector('.qwe')
-let btn = document.querySelector('.btn')
-let show = document.querySelector('.show')
+// let qwe = document.querySelector('.qwe')
+// let btn = document.querySelector('.btn')
+// let show = document.querySelector('.show')
 
 
-let number=0
+// let number=0
 
-    btn.addEventListener('click',()=>{
-    const mycounter = ()=>{
-    number++
-    console.log(number)
-    show.innerHTML=number
-    if (number==qwe.value) {
-        clearInterval(myinterval)
-        number=0
-    }
-}
+//     btn.addEventListener('click',()=>{
+//     const mycounter = ()=>{
+//     number++
+//     console.log(number)
+//     show.innerHTML=number
+//     if (number==qwe.value) {
+//         clearInterval(myinterval)
+//         number=0
+//     }
+// }
 
-    let myinterval = setInterval(() => {
-    mycounter() 
-    }, 100)
-    })
+//     let myinterval = setInterval(() => {
+//     mycounter() 
+//     }, 100)
+//     })
 
 
 
-// let qwe = document.querySelectorAll('.qwe');
-// let btn = document.querySelectorAll('.btn');
-// let show = document.querySelectorAll('.show');
+let qwe = document.querySelectorAll('.qwe');
+let btn = document.querySelectorAll('.btn');
+let show = document.querySelectorAll('.show');
 
-// btn.forEach((button, index) => {
-//     button.addEventListener('click', () => {
-//         let number = 0;
+btn.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        let number = 0;
 
-//         const mycounter = () => {
-//             number++;
-//             console.log(number);
-//             show[index].innerHTML = number;
-//             if (!qwe.value) {
-//                 console.error('set a number')
+        const mycounter = () => {
+            number++;
+            console.log(number);
+            show[index].innerHTML = number;
+            if (qwe.value==='') {
+                console.error('set a number')
                 
-//             }else{
+            }else{
 
-//                 if (number == qwe[index].value) {
-//                     clearInterval(myinterval);
-//                     number = 0;
-//                 }
-//             }
-//         };
-//         let myinterval = setInterval(mycounter, 50);
-//     });
-// });
+                if (number == qwe[index].value) {
+                    clearInterval(myinterval);
+                    number = 0;
+                }
+            }
+        };
+        let myinterval = setInterval(mycounter, 50);
+    });
+});
