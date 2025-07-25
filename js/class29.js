8b9e01b31ab197eb226bd9d9927727e857c97b84
masudrana2803 <masudrana2803@gmail.com>
2025-07-25 22:46:33 +0600

    to-do-list_attribute_set

--- js/class29.js
+++ js/class29.js
@@ -3,35 +3,34 @@ let main = document.querySelector('.main')
 let adding = document.querySelector('.adding')
 let addbutton = document.querySelector('.addbutton');
 let allToDo = document.querySelector('.allToDo')
+let singleToDo = document.querySelector('.singleToDo')
 let error=document.querySelector('.error')
-let totalToDo=document.querySelector('.totalToDo')
-totalToDo.innerHTML=0
-let doneToDo=document.querySelector('.doneToDo')
-doneToDo.innerHTML=0
-let number=0
 
-const handleAdd = () => {
-  console.log('Ha clicked')
-  
-  if(adding.value=='')return(error.innerHTML='Write something please!')
-    
-    number++
-    totalToDo.innerHTML=number
-    error.innerHTML=''
-  // Create tage elements
-  let singleToDo = document.createElement('div')
+let totalToDo=document.querySelector('.totalToDo')
+let totalToDoNo=0
+totalToDo.innerHTML=totalToDoNo
+
+let doneToDo=document.querySelector('.doneToDo')
+let doneToDoNo=0
+doneToDo.innerHTML=doneToDoNo
+
   let singleToDo_input = document.createElement('input')
   let editButton = document.createElement('button')
   let deleteButton = document.createElement('button')
   let doneButton = document.createElement('button')
 
+// add to do 
+const handleAdd = () => {
+  console.log('Ha clicked')
+    // if(adding.value=='')return(error.innerHTML='Write something please!')
 
-
-
-  singleToDo_input.value=adding.value
-  singleToDo_input.setAttribute('readonly', '')
-  adding.value=''
-
+  // Create tage elements
+  // let singleToDo = document.createElement('div')
+  let singleToDo_input = document.createElement('input')
+  let editButton = document.createElement('button')
+  let deleteButton = document.createElement('button')
+  let doneButton = document.createElement('button')
+  
   // Append child
   allToDo.appendChild(singleToDo)
   allToDo.appendChild(singleToDo_input)
@@ -42,7 +41,7 @@ const handleAdd = () => {
 
 
   // classlist add
-  singleToDo.classList.add('singleToDo')
+  // singleToDo.classList.add('singleToDo')
   singleToDo_input.classList.add('singleToDo_input')
   editButton.classList.add('editButton')
   deleteButton.classList.add('deleteButton')
@@ -53,4 +52,62 @@ const handleAdd = () => {
   editButton.innerHTML='<i class="fa-solid fa-pen-to-square"></i>'
   deleteButton.innerHTML='<i class="fa-solid fa-trash"></i>'
   doneButton.innerHTML='<i class="fa-solid fa-check"></i>'
+    
+    totalToDoNo++
+    totalToDo.innerHTML=totalToDoNo
+    error.innerHTML=''
+
+
+
+
+  singleToDo_input.value=adding.value
+  singleToDo_input.setAttribute('readonly', 'readonly')
+  adding.value=''
+
+
+
+  editButton.addEventListener('click', ()=>{
+    console.log('activeEdit')
+    editButton.classList.toggle('activeEdit')
+    if(editButton.classList[1]=='activeEdit'){
+      editButton.innerHTML='<i class="fa-solid fa-floppy-disk"></i>'
+      editButton.style='color:red'
+      singleToDo_input.style='background:rgb(233, 128, 128)'
+      singleToDo_input.removeAttribute('readonly', '')
+    }else{
+      editButton.innerHTML='<i class="fa-solid fa-pen-to-square"></i>'
+      singleToDo_input.setAttribute('readonly', 'readonly')
+      singleToDo_input.style='background:#fff'
+      editButton.style='color:none'
+    }
+  })
+  
+  deleteButton.addEventListener('click',()=>{
+    console.log('clicked')
+    totalToDoNo--
+    totalToDo.innerHTML=totalToDoNo
+
+    // singleToDo_input.parentElement.remove()
+    deleteButton.remove()
+    singleToDo_input.remove()
+    editButton.remove()
+    doneButton.remove()
+
+    if(doneButton.classList[1]=='whydo'){
+    doneToDoNo--
+    doneToDo.innerHTML=doneToDoNo
+    }
+  })
+
+  doneButton.addEventListener('click',()=>{
+    doneButton.classList.add('whydo')
+    singleToDo_input.style='text-decoration: line-through'
+    singleToDo_input.setAttribute('readonly', '')
+    console.log('done it')
+    doneToDoNo++
+    doneToDo.innerHTML=doneToDoNo
+  })
+  
+
 }
+
