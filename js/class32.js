ec26174536d3f2c9b863ae799603fe729e981cc3
masudrana2803 <masudrana2803@gmail.com>
2025-08-06 00:16:39 +0600

    partial complete

--- js/class32.js
+++ js/class32.js
@@ -22,4 +22,36 @@ const listBorder = document.querySelector('.TotalStudentList');
 const addStudent=()=>{
     console.log('ha clicked')
     
+    if (!this.name, !this.roll,!this.id,!this.studentClass,!this.age) {
+        alert('Set all the value first')
+        return;
+        
+    }
+// create tag element
+let ul = document.createElement('ul');
+
+let liName = document.createElement('li');
+// liName.textContent = `Student name:`+ studentName.value;
+liName.textContent = "Student Name"+":"+studentName.value
+ul.appendChild(liName);
+
+
+let liRoll = document.createElement('li');
+liRoll.textContent = `Roll:`;
+ul.appendChild(liRoll);
+
+let liId = document.createElement('li');
+liId.textContent = `Id:`;
+ul.appendChild(liId);
+
+let liClass = document.createElement('li');
+liClass.textContent = `Class:`;
+ul.appendChild(liClass);
+
+let liAge = document.createElement('li');
+liAge.textContent = `Age:`;
+ul.appendChild(liAge);
+
+listBorder.appendChild(ul);
+
 }
